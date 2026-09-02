2559976a8c383d907e7e85eeed3129336ac4486c
sri-kaushik <kaushik.kesanapalli@gmail.com>
2026-09-02 21:12:20 +0530

    remove file persistance code

--- internal/goflow/store.go
+++ internal/goflow/store.go
@@ -78,23 +78,3 @@ func (s *Store) Delete(id string) error {
 	delete(s.jobs, id)
 	return nil
 }
-
-func (s *Store) Save(filename string) error {
-	jobs := s.List()
-	return saveJobs(filename, jobs)
-}
-
-func LoadStore(filename string) (*Store, error) {
-	jobs, err := loadJobs(filename)
-	if err != nil {
-		return nil, err
-	}
-
-	store := NewStore()
-
-	for _, job := range jobs {
-		store.jobs[job.ID] = job
-	}
-
-	return store, nil
-}
